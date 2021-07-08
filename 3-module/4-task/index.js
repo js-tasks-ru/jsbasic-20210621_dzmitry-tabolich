db559dfe853459a6392b9b7e6372a41384af539d
Dzmitry Tabolich <dzmitrytabolich@Dzmitrys-MacBook-Pro.local>
2021-07-08 20:10:51 +0300

    3 module 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,6 +1,4 @@
 function showSalary(users, age) {
-  function showSalary(users, age) {
-    const newAgeArr = users.filter(user => user.age <= age);
-    return newAgeArr.map(user => `${user.name}, ${user.balance}`).join('\n');
-  }
+  const newAgeArr = users.filter(user => user.age <= age);
+  return newAgeArr.map(user => `${user.name}, ${user.balance}`).join('\n');
 }
