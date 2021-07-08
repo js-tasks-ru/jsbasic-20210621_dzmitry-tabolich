6f16547602229af1f15b5da7c9519d465a7c6567
Dzmitry Tabolich <dzmitrytabolich@Dzmitrys-MacBook-Pro.local>
2021-07-08 18:18:50 +0300

    3 модуль пока без 4 таска и 4 модуль полностью

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  let splitArr = str.split("-");
+  let camelizedArr = [];
+
+  splitArr.forEach((item) => {
+    splitArr.indexOf(item) == 0 ?
+      camelizedArr.push(item) :
+      camelizedArr.push(
+        item.replace(item[0], item[0].toUpperCase())
+      );
+
+  }
+  );
+  return camelizedArr.join("");
 }
